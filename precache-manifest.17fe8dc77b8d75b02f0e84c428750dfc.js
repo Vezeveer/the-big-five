38e95f01d8bf7f5b522e0f806085fc30e899137f
vezeveer <mortalmanny@live.com>
2019-02-21 12:11:41 +0800

    initial

---
+++ precache-manifest.17fe8dc77b8d75b02f0e84c428750dfc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "9da731330ed18ded672a",
+    "url": "/static/css/main.7b231f05.chunk.css"
+  },
+  {
+    "revision": "9da731330ed18ded672a",
+    "url": "/static/js/main.9da73133.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "af4df6dac30124a2c909",
+    "url": "/static/css/2.79986b5d.chunk.css"
+  },
+  {
+    "revision": "af4df6dac30124a2c909",
+    "url": "/static/js/2.af4df6da.chunk.js"
+  },
+  {
+    "revision": "ec869f4811f0f7f15e9e7188432ac50e",
+    "url": "/static/media/leopard-1.ec869f48.jpg"
+  },
+  {
+    "revision": "5af7066db98c9768eda908fc5f4438f5",
+    "url": "/static/media/lion-1.5af7066d.jpg"
+  },
+  {
+    "revision": "a7f744cf3df66fb4a6ec44f4167612d3",
+    "url": "/static/media/elephant-1.a7f744cf.jpg"
+  },
+  {
+    "revision": "7c66a66b59212bed8fdebaad940e8a16",
+    "url": "/static/media/buffalo-1.7c66a66b.jpg"
+  },
+  {
+    "revision": "7ea886ae75f2c91039a1bbbd18ef50f0",
+    "url": "/static/media/rhino-1.7ea886ae.jpg"
+  },
+  {
+    "revision": "15f6f2c8bcdfe95ea02bc4c024380a00",
+    "url": "/static/media/logo_transparent_sm2.15f6f2c8.png"
+  },
+  {
+    "revision": "60ae6a0d707a52a696a5b86d9b8ae2cf",
+    "url": "/static/media/bg.60ae6a0d.jpg"
+  },
+  {
+    "revision": "231db12f7cd7a8042faee1a9820767d5",
+    "url": "/index.html"
+  }
+];
